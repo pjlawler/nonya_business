d4b5f553a460e6d1a573ae3ec53f4e3100dc7dc5
Pat Lawler <patlaw777@icloud.com>
2022-03-14 22:01:05 -0500

    added delete options and view department budgets

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,8 @@ CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   department_id INTEGER,
-  salary DEC(9,2) NOT NULL,
-  CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+  salary DEC(9,2),
+  CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees (
@@ -21,6 +21,6 @@ CREATE TABLE employees (
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
   manager_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id)
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
